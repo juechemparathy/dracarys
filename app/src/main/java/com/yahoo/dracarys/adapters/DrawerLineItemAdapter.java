2dcea0a6f41f5d212f9de5937570e45759d3c316
Jue <jue@yahoo-inc.com>
2015-02-10 05:47:25 -0800

    navigation drawer update

--- app/src/main/java/com/yahoo/dracarys/adapters/DrawerLineItemAdapter.java
+++ app/src/main/java/com/yahoo/dracarys/adapters/DrawerLineItemAdapter.java
@@ -1,12 +1,18 @@
 package com.yahoo.dracarys.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yahoo.dracarys.R;
+import com.yahoo.dracarys.activities.LoginActivity;
+import com.yahoo.dracarys.activities.ProfileActivity;
 import com.yahoo.dracarys.models.DrawerLineItem;
 
 import java.util.List;
@@ -14,19 +20,22 @@ import java.util.List;
 /**
  * Created by jue on 1/31/15.
  */
-public class DrawerLineItemAdapter extends RecyclerView.Adapter<DrawerLineItemViewHolder>{
+public class DrawerLineItemAdapter extends RecyclerView.Adapter<DrawerLineItemAdapter.DrawerLineItemViewHolder> {
 
 
     private LayoutInflater layoutInflater;
     private List<DrawerLineItem> data;
-    public DrawerLineItemAdapter(Context context,List<DrawerLineItem> data){
+    private Context context;
+
+    public DrawerLineItemAdapter(Context context, List<DrawerLineItem> data) {
+        this.context = context;
         this.layoutInflater = LayoutInflater.from(context);
-        this.data=data;
+        this.data = data;
     }
 
     @Override
     public DrawerLineItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View lineItemView = layoutInflater.inflate(R.layout.drawer_lineitem,parent,false);
+        View lineItemView = layoutInflater.inflate(R.layout.drawer_lineitem, parent, false);
         DrawerLineItemViewHolder viewHolder = new DrawerLineItemViewHolder(lineItemView);
         return viewHolder;
     }
@@ -42,4 +51,33 @@ public class DrawerLineItemAdapter extends RecyclerView.Adapter<DrawerLineItemVi
     public int getItemCount() {
         return data.size();
     }
+
+
+    //ViewHolder for this adapter
+    class DrawerLineItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView title;
+        ImageView icon;
+
+        public DrawerLineItemViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.tv_drawer);
+            icon = (ImageView) itemView.findViewById(R.id.iv_drawer);
+            icon.setOnClickListener(this);
+            title.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getPosition();
+            //Handle this more elegantly
+            if (position == 0) {
+                context.startActivity(new Intent(context, LoginActivity.class));
+            }if (position == 1) {
+                context.startActivity(new Intent(context, ProfileActivity.class));
+            } else {
+                Toast.makeText(context, "Clicked on position " + position, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
