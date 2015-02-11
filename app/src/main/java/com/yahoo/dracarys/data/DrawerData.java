d1c766f57fb6b957ba8e18ce6f913f721c8a8a26
Jue <jue@yahoo-inc.com>
2015-02-11 11:55:01 -0800

    Update drawer data

--- app/src/main/java/com/yahoo/dracarys/data/DrawerData.java
+++ app/src/main/java/com/yahoo/dracarys/data/DrawerData.java
@@ -15,12 +15,37 @@ public class DrawerData {
     public static List<DrawerLineItem> getDrawerItems() {
         if (data == null || data.size() == 0) {
             data = new ArrayList();
-            for (int i = 0; i < 20; i++) {
-                DrawerLineItem lineItem = new DrawerLineItem();
-                lineItem.setIconId(R.drawable.ic_launcher);
-                lineItem.setTitle("Hello World " + i);
-                data.add(lineItem);
-            }
+
+            DrawerLineItem lineItem = new DrawerLineItem();
+            lineItem.setIconId(R.drawable.book_search);
+            lineItem.setTitle("Search Books");
+            data.add(lineItem);
+
+            lineItem = new DrawerLineItem();
+            lineItem.setIconId(R.drawable.book_stack);
+            lineItem.setTitle("Timeline");
+            data.add(lineItem);
+
+            lineItem = new DrawerLineItem();
+            lineItem.setIconId(R.drawable.book_profile);
+            lineItem.setTitle("My Profile");
+            data.add(lineItem);
+
+            lineItem = new DrawerLineItem();
+            lineItem.setIconId(R.drawable.book_bookmark);
+            lineItem.setTitle("Notifications");
+            data.add(lineItem);
+
+            lineItem = new DrawerLineItem();
+            lineItem.setIconId(R.drawable.book_star_filled);
+            lineItem.setTitle("Favorites");
+            data.add(lineItem);
+
+            lineItem = new DrawerLineItem();
+            lineItem.setIconId(R.drawable.book_image_scanner);
+            lineItem.setTitle("Scan ISBN");
+            data.add(lineItem);
+
         }
         return data;
     }
